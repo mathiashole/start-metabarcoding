c29cfd671d31373501fe36366e7206d159847625
mathiashole <joacomangino@gmail.com>
2026-08-31 08:16:37 -0300

    Update introduction path

--- introduction.R
+++ introduction.R
@@ -2,3 +2,12 @@
 
 # Charge library
 #------------------------------------------------------------------------
+
+R.version.string
+getwd()
+
+# Todo lo que descarguemos quedará dentro de la carpeta "data".
+dir.create("/home/jmangino/metabarcoding/data", showWarnings = FALSE)
+
+setwd("/home/jmangino/metabarcoding/data")
+
