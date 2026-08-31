f309914d1fe9a62417a2b752315f7c2e0674a52a
mathiashole <joacomangino@gmail.com>
2026-08-31 14:42:38 -0300

    Update introduction read data

--- introduction.R
+++ introduction.R
@@ -7,7 +7,7 @@ R.version.string
 getwd()
 
 # Todo lo que descarguemos quedará dentro de la carpeta "data".
-dir.create("/home/jmangino/metabarcoding/data", showWarnings = FALSE)
+#dir.create("/home/jmangino/metabarcoding/data", showWarnings = FALSE)
 
 setwd("/home/jmangino/metabarcoding/data")
 
@@ -21,4 +21,33 @@ drive_deauth() # No hace falta iniciar sesión porque la carpeta es pública.
 
 carpeta_taller <- as_id(
   "https://drive.google.com/drive/folders/1SOhCrJXgrtucWavxsxizLY4FErL3Almk"
+)
+
+archivos_principales <- drive_ls(carpeta_taller)
+archivos_principales
+
+metadata_local <- file.path("/home/jmangino/metabarcoding/data", "sampleData_alimentos.csv")
+
+# Descargamos la metadata solamente si todavía no está en la computadora.
+if (!file.exists(metadata_local)) {
+  metadata_drive <- archivos_principales[
+    archivos_principales$name == "sampleData_alimentos.csv",
+  ]
+
+  if (nrow(metadata_drive) == 0) {
+    stop("No se encontró sampleData_alimentos.csv en Google Drive.")
+  }
+
+  drive_download(
+    metadata_drive,
+    path = metadata_local,
+    overwrite = FALSE
+  )
+}
+
+# Leemos la tabla.
+metadata <- read.csv(
+  metadata_local,
+  stringsAsFactors = FALSE,
+  na.strings = c("", "NA")
 )
