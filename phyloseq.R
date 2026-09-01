e9acbf0576d1d134c20c1f6c4d1385e18f818b1a
mathiashole <joacomangino@gmail.com>
2026-09-01 11:28:54 -0300

    Update read data

--- phyloseq.R
+++ phyloseq.R
@@ -2,3 +2,13 @@
 
 # Charge library
 #------------------------------------------------------------------------
+
+if (!requireNamespace("phyloseq", quietly = TRUE)) {
+  BiocManager::install("phyloseq")
+}
+
+library(phyloseq)
+library(ggplot2)
+library(dplyr)
+
+set.seed(123)
